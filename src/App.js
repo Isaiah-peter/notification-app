5a6180f009908ba74001ce2edf5a7afe9ba91e60
Peter Isaiah <peterisaiah4fun@gmail.com>
2021-11-02 20:30:48 +0100

    more changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import "./App.css";
+import React, { useState } from "react";
+import Card from "./component/card/Card";
+import Navbar from "./component/navbar/Navbar";
 
 function App() {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState("");
+  console.log(user);
+
   return (
     <div className="container">
-      <div className="login">
-        <input type="text" placeholder="username" />
-        <button>Login </button>
-      </div>
+      {user ? (
+        <>
+          <Navbar />
+          <Card />
+        </>
+      ) : (
+        <div className="login">
+          <input
+            type="text"
+            placeholder="username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <button onClick={() => setUser(username)}>Login </button>
+        </div>
+      )}
     </div>
   );
 }
